f1ace154501b565772097c4eb6256388f2fe1604
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-04-18 17:01:03 +0200

    Q_19

--- linq_02/Program.cs
+++ linq_02/Program.cs
@@ -143,11 +143,18 @@ namespace linq_02
             #region Q_18
             //18. Generate a multiplication table row for 7
 
-          //  var result = Enumerable.Range(1, 10).Select(x => $"7 x {x} = {7 * x}");
+            //  var result = Enumerable.Range(1, 10).Select(x => $"7 x {x} = {7 * x}");
 
             #endregion
 
 
+            #region Q_19
+            //19. Generate even numbers between 1 and 30.
+
+            //var result = Enumerable.Range(1, 30).Where(x => x % 2 == 0);
+            #endregion
+
+            
 
             //foreach (var item in result)
             //{
