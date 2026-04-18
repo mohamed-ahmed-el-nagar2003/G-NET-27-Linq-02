a92915f8f07d9f0049da3743d572d544276ef80a
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-04-18 15:07:20 +0200

    Q_02

--- linq_02/Program.cs
+++ linq_02/Program.cs
@@ -6,7 +6,13 @@
         {
             #region Q_01
             //1. Get top 3 most expensive products
-         //   var result = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+            //   var result = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+
+            #endregion
+
+            #region Q_02
+            //2. show page 2 of products, with page size = 5
+            //var result = Source.ProductList.Skip(5).Take(5);
 
             #endregion
 
